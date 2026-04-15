2098bb49d61bacfbe01293743c17db8e06172eef
User <pereraniveka@gmail.com>
2026-04-15 11:08:08 +0530

    Chebyshev distance function

--- src/BestFirstSearch.java
+++ src/BestFirstSearch.java
@@ -1,2 +1,16 @@
 public class BestFirstSearch {
+    private Maze maze;
+
+    public BestFirstSearch(Maze maze){
+        this.maze = maze;
+    }
+
+    //Chebyshev distance heuristic
+    private int chebyshev(int nodeId){
+        int nx = maze.nodes[nodeId][0];
+        int ny = maze.nodes[nodeId][1];
+        int gx = maze.nodes[maze.goalNode][0];
+        int gy = maze.nodes[maze.goalNode][1];
+        return Math.max(Math.abs(nx-gx), Math.abs(ny-gy));
+    }
 }
